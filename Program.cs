9b8fdbf29fbc48a7a1ae3f593b4d9ccad25d4403
Ulugbek1904 <julugbek023@gmail.com>
2024-06-24 17:43:19 +0500

    Add function

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using PhoneBookApp.Services;
+using System;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to the Phonebook Console Application\n");
+            bool continueProg = true;
+
+            while (continueProg)
+            {
+                Console.WriteLine("\t\t\t\t\tMenu:");
+                Console.WriteLine("1. My contacts");
+                Console.WriteLine("2. Add contact");
+                Console.WriteLine("3. Edit contact");
+                Console.WriteLine("4. Delete contact");
+                Console.WriteLine("5. Exit Program\n");
+
+                Console.WriteLine("Choose you wanted:");
+                try
+                {
+                    string input = Console.ReadLine();
+                    int userInput = int.Parse(input);
+
+                    switch (userInput)
+                    {
+                        case 1:
+                            ContactServices.ShowAllContacts();
+                            break;
+                        case 2:
+                            ContactServices.AddContact();
+                            ContactServices.ShowAllContacts();
+                            break;
+                        case 3:
+                            ContactServices.EditContact();
+                            ContactServices.ShowAllContacts();
+                            break;
+                        case 4:
+                            ContactServices.DeleteContact();
+                            ContactServices.ShowAllContacts();
+                            break;
+                        case 5:
+                            continueProg = false;
+                            Console.Clear();
+                            break;
+                        default:
+                            Console.WriteLine("Enter a number from 1 to 5.");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a numeric value. Try again!!!");
+                }
+            }
+        }
+    }
+}
---
+++ Services/ContactServices.cs
@@ -0,0 +1,96 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PhoneBookApp.Services
+{
+    /// <summary>
+    /// bu class contact lar ustida amallar bajarishimizni amalga oshiradi
+    /// </summary>
+    public class ContactServices
+    {
+        private static List<PhoneContact> phoneContacts = new List<PhoneContact>();
+
+        /// <summary>
+        /// bu Method bizga contaclarimizni console oynasiga chiqarib beradi. agar ularninig soni 0 bo'lsa empty so'zi bilan
+        /// </summary>
+        public static void ShowAllContacts()
+        {
+            if (phoneContacts.Count == 0)
+            {
+                Console.WriteLine("Your contacts list is empty.\n");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Your contact list :");
+                for (int i = 0; i < phoneContacts.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. Name: {phoneContacts[i].Name}, Number: {phoneContacts[i].PhoneNumber}\n");
+                }
+            }
+
+        }
+        /// <summary>
+        /// bunda biz contact listga yangi contact qo'sha olamiz.
+        /// </summary>
+        public static void AddContact()
+        {
+            Console.Write("Enter name: ");
+            string newName = Console.ReadLine();
+
+            Console.Write("Enter number: ");
+            string newNumber = Console.ReadLine();
+
+            phoneContacts.Add(new PhoneContact { Name = newName, PhoneNumber = newNumber });
+            Console.WriteLine("Contact added successfully!\n");
+        }
+        /// <summary>
+        /// contactlarni edit qilish uchun contatct index ini so'raydi va yangilanishni kiritishni talab qiladi.
+        /// </summary>
+        public static void EditContact()
+        {
+            ShowAllContacts();
+            Console.Write("Enter the index of the contact you want to edit: ");
+            int index;
+            // biz foydalanuvchidan input so'raymiz va uni int tipiga o'tkazishga harakayt qilamiz, o'tsa uni index ga saqlaymiz.
+            if (int.TryParse(Console.ReadLine(), out index) && index > 0 && index <= phoneContacts.Count)
+            {
+                // bizda index bo'yicha ketganda phoneContacts[index] 0 dan boshlanadi shuning uchun 1 ni ayirib qo'yamiz.
+                index--;
+                Console.Write("Enter new name: ");
+                phoneContacts[index].Name = Console.ReadLine();
+
+                Console.Write("Enter new number: ");
+                phoneContacts[index].PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully!\n");
+            }
+            else // agar input int tipiga o'ta olmasa invalid chiqaradi!
+            {
+                Console.WriteLine("Invalid contact number.");
+            }
+        }
+        /// <summary>
+        /// bizga keraksiz bo'lgan contactlarni indexiga qarab o'chirib yuboradi.
+        /// </summary>
+        public static void DeleteContact()
+        {
+            ShowAllContacts();
+            Console.Write("Enter the index of the contact you want to delete: ");
+            int index;
+            // input qabul qiladi int ga o'tkazadi oraliqlarni tekshiradi
+            if (int.TryParse(Console.ReadLine(), out index) && index > 0 && index <= phoneContacts.Count)
+            {
+                index--; // berilgan sonni index ga tenglash uchun 1 ayirib tashlaydi.
+                phoneContacts.RemoveAt(index); // RemoveAt index bo'yicha o'chiradi.
+                Console.WriteLine("Contact deleted successfully!\n");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid contact number.");
+            }
+        }
+    }
+}
